a87191d0ce7839c47e2879cb4b09a618947997c9
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-10-05 09:03:26 -1200

    Add files via upload

---
+++ Invodata.jsx
@@ -0,0 +1,18 @@
+
+function Add(){
+    return(
+        <>
+        <div className="flex items-center bg-gray-800 w-[700px] border hover:border-blue-700 h-28 mb-4 rounded-lg"> 
+            <div className="flex justify-between w-full px-10">
+                 <h1 className="text-white  font-bold text-lg">#03447928295</h1>
+                <p className="text-xs text-center items-center text-gray-500 font-semibold">31-12-2022</p>
+                <p className="text-white font-bold mb-2 text-2xl">$3.00</p>
+                <button className="bg-gray-950 font-semibold text-white w-32 h-12 rounded-md">
+          draft
+          </button>
+            </div>
+        </div>
+ </>
+    );
+}
+export default Add;
