fc0bf01386dceacfd7d5fdb1d570db162a01c364
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-10-08 10:27:21 -1200

    Update Data.js

---
+++ Data.js
@@ -0,0 +1,27 @@
+import { create } from "zustand";
+
+export const useStore = create((set) => ({
+  formData: {
+    address: "",
+    city: "",
+    code: "",
+    country: "",    
+    date: "",
+    name: "",
+    email: "",
+  },
+  setFormData: (newFormData) =>
+    set((state) => ({
+      formData: { ...state.formData, ...newFormData },
+    })),
+}));
+
+
+export const useTodos = create((set) => ({
+    list: [],
+    setList: (newList) =>
+        set((state) => ({
+            list: newList,
+        })),
+    }));
+  
