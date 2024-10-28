bebbee790c91ea9104046f95711f29df0a0b29e3
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-10-28 20:20:57 +0500

    Update From.jsx

--- From.jsx
+++ From.jsx
@@ -1,67 +1,179 @@
-import { useState } from "react";
-import { Menu, MenuButton, MenuItem, MenuItems,MenuSection } from "@headlessui/react";
-import down from "./img/down.svg";
+import { useState, useEffect } from "react";
+import Add from "./Add";
+import { useStore, useTodos } from "./Data";
 
-function Input() {
-  const [selectedStatus, setSelectedStatus] = useState(""); 
+function Form(props) {
+  const { formData, setFormData } = useStore();
+  const { list, setList } = useTodos();
+  const [open, setOpen] = useState([]);
+  const [showlist, setShowlist] = useState();
 
-  const handleCheckboxChange = (value) => {
-    setSelectedStatus(value);
+  const openAdd = () => {
+    setOpen([...open, {}]);
   };
 
+  const openDelete = (key) => {
+    const openDeleteArray = open.filter((_, i) => i !== key);
+    setOpen(openDeleteArray);
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const saveToLocalStorage = () => {
+    localStorage.setItem("formData", JSON.stringify(formData));
+    alert("Form data saved to local storage!");
+  };
+
+  useEffect(() => {
+    const savedFormData = localStorage.getItem("formData");
+    if (savedFormData) {
+      setFormData(JSON.parse(savedFormData));
+    }
+  }, [setFormData]);
   return (
     <>
-    
-      <Menu>
-        <MenuButton>
-          <div className="flex items-center">
-          <p className="text-md font-semibold">Filter by status</p>
-          <button>
-            <img src={down} width={25} height={25} className="pt-2"></img>
-          </button>
-          </div>        
-        </MenuButton>
-        <div className=""> 
-        <MenuItems anchor="bottom" className="border border-gray-700 rounded-lg p-3" >
-          <MenuItem>
-            <div className="flex flex-row m-2">
-              <input className="bg-transparent text-white"
-                type="checkbox"
-                value="Pending"
-                checked={selectedStatus === "Pending"}
-                onChange={() => handleCheckboxChange("Pending")}
-              ></input>
-              <p className="text-xs pl-3 font-semibold">Pending</p>
-            </div>
-          </MenuItem>
-          <MenuItem>
-            <div className="flex flex-row m-2">
+      <section>
+        <div className="container w-fit h-screen overflow-y-auto bg-gray-900 rounded-lg">
+          <div className="ml-28 mr-4">
+            <div className="container p-4">
+              <h1 className="text-indigo-600 w-20 font-semibold">Bill From</h1>
+              <p className="text-gray-500 mb-2 font-semibold">Street Address</p>
               <input
-                type="checkbox"
-                value="Paid"
-                checked={selectedStatus === "Paid"}
-                onChange={() => handleCheckboxChange("Paid")}
-              ></input>
-              <p className="text-xs pl-3 font-semibold">Paid</p>
+                type="text"
+                value={formData.address}
+                name="address"
+                placeholder="Street Address"
+                onChange={handleInputChange}
+                className="bg-gray-800 text-sm rounded-md w-[615px] h-10 p-4"
+              />{" "}
+              {props.zulfi}
+              <div className="container flex flex-row mt-5">
+                <div>
+                  <p className="text-gray-500 mb-2 font-semibold">City</p>
+                  <input
+                    value={formData.city}
+                    name="city"
+                    type="text"
+                    placeholder="City"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md mr-5 w-42 h-10 p-4"
+                  />
+                </div>
+                <div>
+                  <p className="text-gray-500 mb-2 font-semibold">Post Code</p>
+                  <input
+                    value={formData.code}
+                    name="code"
+                    type="number"
+                    placeholder="Post Code"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md mr-5 w-42 h-10 p-4"
+                  />
+                </div>
+                <div>
+                  <p className="text-gray-500 mb-2 font-semibold">Country</p>
+                  <input
+                    value={formData.country}
+                    name="country"
+                    type="text"
+                    placeholder="Country"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md w-42 h-10 p-4"
+                  />
+                </div>
+              </div>
+              <div className="mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">Invoice Date</p>
+                <input
+                  value={formData.date}
+                  name="date"
+                  type="date"
+                  placeholder="mm/dd/yyyy"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm rounded-md w-[615px] h-10 p-4"
+                />
+              </div>
+              <h1 className="text-indigo-600 w-20 mt-10 font-semibold">
+                Bill to
+              </h1>
+              <div className="mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">
+                  Client's Name
+                </p>
+                <input
+                  value={formData.name}
+                  name="name"
+                  type="text"
+                  placeholder="Client's Name"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm mb-2 rounded-md w-[615px] h-10 p-4"
+                />
+              </div>
+              <div className="mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">
+                  Client's Email
+                </p>
+                <input
+                  value={formData.email}
+                  name="email"
+                  type="text"
+                  placeholder="Client's Email"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm rounded-md w-[615px] h-10 p-4"
+                />
+              </div>
+              {open.map((_, index) => (
+                <Add
+                  key={index}
+                  i={index}
+                  handleDelete={() => openDelete(index)}
+                />
+              ))}
+              <button
+                onClick={openAdd}
+                className="bg-transparent text-indigo-600 text-md font-semibold hover:bg-white hover:scale-100 rounded-full w-[615px] h-10 mt-4"
+              >
+                + Add new Item
+              </button>
+              <div className="flex flex-row items-center pt-4 justify-between">
+                <button className="text-gray-800 font-semibold bg-gray-300 ml-16 w-32 h-12 rounded-full">
+                  Discord
+                </button>
+                <div>
+                  <button className="bg-gray-800 font-semibold text-white w-32 h-12 mr-2 rounded-full">
+                    Save as Draft
+                  </button>
+                  <button
+                    onClick={() => {
+                      props.handleSaveClick();
+                      props.setOpen(false);
+                      saveToLocalStorage();
+                      setList([...list, formData]);
+                      setFormData({
+                        address: "",
+                        city: "",
+                        code: "",
+                        country: "",
+                        date: "",
+                        name: "",
+                        email: "",
+                      });
+                    }}
+                    className="bg-indigo-700 font-semibold text-white w-32 h-12 rounded-full"
+                  >
+                    Save & Send
+                  </button>
+                </div>
+              </div>
             </div>
-          </MenuItem>
-          <MenuItem>
-            <div className="flex flex-row m-2">
-              <input
-                type="checkbox"
-                value="Draft"
-                checked={selectedStatus === "Draft"}
-                onChange={() => handleCheckboxChange("Draft")}
-              ></input>
-              <p className="text-xs pl-3 font-semibold">Draft</p>
-            </div>
-          </MenuItem>
-        </MenuItems>
+          </div>
         </div>
-      </Menu>
-     
+      </section>
     </>
   );
 }
 
-export default Input;
+export default Form;
