e4c44fcb817ae674e5307184e76e429b5f6e93d7
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2024-11-12 20:45:57 +0500

    Update Fromin.jsx

--- Fromin.jsx
+++ Fromin.jsx
@@ -1,154 +1,190 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Add from "./Add.jsx";
+import { useEidt, useUpdate } from "./useEidt.js";
 import { useTodos } from "./useTodos.js";
-import { useEidt } from "./useEidt.js";
-import { useNavigate, useParams } from "react-router-dom";
-import side from "./img/side.png";
-import dark from "./img/dark.svg";
-import man from "./img/man.jpg";
-import Alart from "./Alart.jsx";
-import Newfrom from "./Newfrom.jsx";
-function Fromin() {
-  const { list, setListUpdate } = useTodos();
-  const [back, setback] = useState(false);
-  const [deleteAlart, setDeleteAlart] = useState(false);
-  const [showAlert, setShowAlert] = useState(false);
-  const [markPaid,setMarkpaid ] = useState();
-  const { isEdit, handleEidt } = useEidt();
- 
-  const handleChangeStatus = () => {
-    setMarkpaid(!markPaid);
-    if (markPaid) {
-      setListUpdate("Paid");
-    }
+import toast from "react-hot-toast";
+
+function Newfrom({ id }) {
+  const [open, setOpen] = useState([]);
+  const { handleEidt } = useEidt();
+  const { formUpdateData, setFormUpdateData } = useUpdate();
+  const { setEidtUpdate } = useTodos();
+
+  const openAdd = () => {
+    setOpen([...open, {}]);
+  };
+  const openDelete = (key) => {
+    const openDeleteArray = open.filter((_, i) => i !== key);
+    setOpen(openDeleteArray);
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormUpdateData({ ...formUpdateData, [name]: value });
+  };
+  const saveUpdate = () => {
+    toast("Update data");
   };
 
-
-  const handleDeleteAlart = () => {
-    setDeleteAlart(deleteAlart);
-  };
-
-  const navigate = useNavigate();
-  const handleFrominBack = () => {
-    setback(!back);
-  };
-
-  const handleShowAlert = () => {
-    setShowAlert(true);
-  };
-
-  const handleCloseAlert = () => {
-    setShowAlert(false);
-  };
-
-  
- 
-
-  const params = useParams();
-  const id = params.id;
-  const filteredData = list.find((v) => v.id === Number(id));
-  console.log("formData", filteredData);
+  console.log("formUpdateData", formUpdateData);
   return (
     <>
-      {" "}
-     {true === isEdit &&  <div className="absolute z-20"><Newfrom /></div>}
-      <div className="absolute z-20">
-      <nav className="container">
-          <div className="bg-slate-700 w-auto h-auto flex flex-col rounded-tr-3xl justify-between">
-            <img src={side} width={120} height={120} className="mb-10"></img>
-            <div className="flex justify-center mt-80">
-              <button>
-                <img src={dark} width={25} height={25} className="mb-5"></img>
-              </button>
-            </div>
-          </div>
-          <div className="bg-slate-700 flex justify-center rounded-br-3xl mt-[1px]">
-            <img
-              src={man}
-              width={30}
-              height={30}
-              className="rounded-full m-[31px]"
-            ></img>
-          </div>
-      </nav>
-      </div>
-      <section className=" flex flex-col items-center w-screen justify-center">
-        <button
-          onClick={() => navigate("/")}
-          className="bg-transparent flex font-semibold text-white text-start justify-center items-center w-32 h-12 rounded-md"
-        >
-          Go back
-        </button>
-
-        <div className="flex justify-between w-[700px] rounded-xl mb-5 h-20 bg-gray-900">
-          <div className="flex flex-row items-center pt-2 justify-between">
-            <div className="flex text-center items-center">
-              <p className="text-white p-4 font-semibold">Status</p>
-              <p className="bg-gray-950 font-semibold text-white w-32 flex justify-center text-center items-center h-12 rounded-md">
-              {markPaid ? "Paid" : filteredData?.type}
-              </p>
-            </div>
-            <div className="ml-20">
-              <button onClick={handleEidt} className="text-gray-800 font-semibold text-sm bg-white w-28 h-11 rounded-full">
-                Eidt
-              </button>
-              <button
-                onClick={handleDeleteAlart}
-                className="bg-red-800 font-semibold text-md ml-2 text-white w-28 h-11 rounded-full"
-              >
+      <section>
+        <div className="container w-fit h-screen overflow-y-auto bg-gray-900 rounded-lg">
+          <div className="md:ml-28 mr-4">
+            <div className=" container p-1 lg:p-4">
+              <div className="flex justify-between">
+                <h1 className="text-indigo-600 w-full font-semibold">
+                  Bill From
+                </h1>
+                <button onClick={handleEidt}>
+                  <svg
+                    class="w-6 h-6 text-800 dark:text-white"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M6 18 17.94 6M18 18 6.06 6"
+                    />
+                  </svg>
+                </button>
+              </div>
+              <p className="text-gray-500 mb-2 font-semibold">Street Address</p>
+              <input
+                type="text"
+                value={formUpdateData.address}
+                name="address"
+                placeholder="Street Address"
+                onChange={handleInputChange}
+                className="bg-gray-800 text-sm rounded-md w-full  md:h-10 p-1 lg:p-4"
+              />{" "}
+              <div className="container grid grid-rows-3 md:grid-rows-1 md:grid-cols-3 gap-3 mt-5">
                 <div>
-                  <button onClick={handleShowAlert}>Delete</button>
-                  <Alart
-                    isOpen={showAlert}
-                    onClose={handleCloseAlert}
-                    id={id}
+                  <p className="text-gray-500 mb-2 font-semibold">City</p>
+                  <input
+                    value={formUpdateData.city}
+                    name="city"
+                    type="text"
+                    placeholder="City"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md mr-5 w-full h-10 p-1 lg:p-4"
                   />
                 </div>
+                <div>
+                  <p className="text-gray-500 mb-2 font-semibold">Post Code</p>
+                  <input
+                    value={formUpdateData.code}
+                    name="code"
+                    type="number"
+                    placeholder="Post Code"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md mr-5 w-full h-10 p-1 lg:p-4"
+                  />
+                </div>
+                <div>
+                  <p className="text-gray-500 mb-2 font-semibold">Country</p>
+                  <input
+                    value={formUpdateData.country}
+                    name="country"
+                    type="text"
+                    placeholder="Country"
+                    onChange={handleInputChange}
+                    className="bg-gray-800 text-sm rounded-md w-full h-10 p-1 lg:p-4"
+                  />
+                </div>
+              </div>
+              <div className="mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">Invoice Date</p>
+                <input
+                  value={formUpdateData.date}
+                  name="date"
+                  type="date"
+                  placeholder="mm/dd/yyyy"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm rounded-md w-full h-6 md:h-10 p-2 lg:p-4"
+                />
+              </div>
+              <h1 className="text-indigo-600 w-20 mt-2 lg:mt-10 font-semibold">
+                Bill to
+              </h1>
+              <div className="mt-2 lg:mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">
+                  Client's Name
+                </p>
+                <input
+                  value={formUpdateData.name}
+                  name="name"
+                  type="text"
+                  placeholder="Client's Name"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm mb-2 rounded-md  w-full h-6 md:h-10 p-2 lg:p-4"
+                />
+              </div>
+              <div className="mt-2 md:mt-5">
+                <p className="text-gray-500 mb-2 font-semibold">
+                  Client's Email
+                </p>
+                <input
+                  value={formUpdateData.email}
+                  name="email"
+                  type="text"
+                  placeholder="Client's Email"
+                  onChange={handleInputChange}
+                  className="bg-gray-800 text-sm rounded-md   w-full h-6 md:h-10 p-2 lg:p-4"
+                />
+              </div>
+              {open.map((_, index) => (
+                <Add
+                  key={index}
+                  i={index}
+                  handleDelete={() => openDelete(index)}
+                />
+              ))}
+              <button
+                onClick={openAdd}
+                className="bg-transparent text-indigo-600 text-md font-semibold hover:bg-white hover:scale-100 rounded-full w-72  md:w-[615px] h-6 md:h-10 mt-4"
+              >
+                + Add new Item
               </button>
-              <button onClick={handleChangeStatus}  className="bg-indigo-700 font-semibold text-sm text-white w-32 h-12 ml-2 rounded-full">
-                Save & Paid
-              </button>
-            </div>
-          </div>
-        </div>
-        <div className="flex flex-col justify-center w-[700px] rounded-xl mb-5 h-auto bg-gray-900">
-          <div className="flex flex-row justify-between">
-            <div className="flex flex-col m-6">
-              <h1 className="text-md font-semibold">Invoice ID</h1>
-              <p className="text-sm text-gray-400">{id}</p>
-              <h1 className="text-md font-semibold">Name</h1>
-              <p className="text-sm text-gray-400">{filteredData?.name}</p>
-              <h1 className="text-md font-semibold">Post Code</h1>
-              <p className="text-sm text-gray-400">{filteredData?.code}</p>
-              <h1 className="text-md font-semibold">Invoice date</h1>
-              <p className="text-sm text-gray-400">{filteredData?.date}</p>
-            </div>
-            <div className="flex flex-col m-6">
-              <p className="text-sm text-gray-400">
-                {filteredData?.address} <br />
-                {filteredData?.city} <br />
-                {filteredData?.country} <br />
-                {filteredData?.email}
-              </p>
-            </div>
-          </div>
-          <div className="flex flex-col  m-4 bg-gray-800">
-            <div className="flex justify-between w-fill m-6">
-              <p className="text-white mb-2 font-semibold">Item Name</p>
-              <p className="text-white mb-2 font-semibold">Qty.</p>
-              <p className="text-white mb-2 font-semibold">Price</p>
-              <p className="text-white mb-2 font-semibold">Total</p>
-            </div>
-            <div className="flex flex-row w-fix justify-between m-6">
-              <p className="text-sm text-gray-400">no data</p>
-              <p className="text-sm text-gray-400">no data</p>
-              <p className="text-sm text-gray-400">no data</p>
-              <p className="text-sm text-gray-400">no data</p>
-            </div>
-            <div className="flex flex-row w-full  justify-between  bg-gray-700">
-              <p className="text-white flex text-center items-center justify-center mb-2 pl-8 font-semibold">
-                Total
-              </p>
-              <p className="text-white font-bold mb-2 pr-4 text-2xl">$3.00</p>
+              <div className="flex flex-row items-center pt-4 justify-between">
+                <button
+                  onClick={() => {
+                    handleEidt();
+                  }}
+                  className="text-gray-800 font-semibold bg-gray-300 md:ml-16 text-xs p-2 md:w-32 md:h-12 rounded-full"
+                >
+                  Cancel
+                </button>
+
+                <button
+                  onClick={() => {
+                    handleEidt();
+                    saveUpdate();
+                    setEidtUpdate(id, formUpdateData);
+                    setFormUpdateData({
+                      id: "",
+                      address: "",
+                      city: "",
+                      code: "",
+                      country: "",
+                      date: "",
+                      name: "",
+                      email: "",
+                      type: "save",
+                    });
+                  }}
+                  className="bg-indigo-700 font-semibold text-white w-32 h-12 rounded-full"
+                >
+                  Save Changes
+                </button>
+              </div>
             </div>
           </div>
         </div>
@@ -157,4 +193,4 @@ function Fromin() {
   );
 }
 
-export default Fromin;
+export default Newfrom;
